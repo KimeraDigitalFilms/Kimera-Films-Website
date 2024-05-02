a83483892e85c036c7a3f217ee24b51fc044bfeb
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-05-02 22:06:43 +0530

    ...

--- src/components/Ticker/Ticker.jsx
+++ src/components/Ticker/Ticker.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { AnimatePresence, motion, } from "framer-motion";
+import { motion,AnimatePresence } from "framer-motion";
 import './Ticker.css'
 
 function TickerComponent() {
