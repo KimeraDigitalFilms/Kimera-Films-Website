3bc1c65444107861248d7b86d0f89265cc865ec2
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-05-09 13:00:54 +0530

    ...

--- src/components/ShowReel/ShowReelModal.jsx
+++ src/components/ShowReel/ShowReelModal.jsx
@@ -25,7 +25,7 @@ const ShowReelModal = ({ isOpen, setIsOpen }) => {
              rounded-lg w-[800px] shadow-xl cursor-default relative "
           >
             <div className="relative z-10 w-full h-full object-cover">
-              <video
+              {/* <video
                 preload="none"
                 poster="/thumbnails/random3.png"
                 loop
@@ -35,7 +35,23 @@ const ShowReelModal = ({ isOpen, setIsOpen }) => {
                 controls
                 src="/videos/random3.mp4"
                 className="w-full hover:cursor-pointer h-full"
-              ></video>
+              ></video> */}
+              <div style={{position:'relative',paddingLeft:'50%', paddingBottom:'35%',paddingTop:'35%',paddingRight:'50%', width: "25vw !important", height: "25vh !important",overflowX:'hidden' }}
+          className="-z-10"
+          >
+          <iframe
+          id="reel"
+            src="https://player.vimeo.com/video/938376141?h=4bb8442f8d&autoplay=1&loop=1&title=0&byline=0&muted=0&controls=1"
+            // style={{ position:'relative',width: "25vw !important", height: "25vh !important",padding:0 }}
+            style={{position:"absolute",top:"0",left:"0",width:"100%",height:"100%"}}
+            // className="w-full h-full"
+            frameborder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            webkitallowfullscreen 
+            mozallowfullscreen
+            allowfullscreen
+            ></iframe>
+            </div>
             </div>
           </motion.div>
         </motion.div>
