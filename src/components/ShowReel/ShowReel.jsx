fe7495e274f431f1040b2ecefc9a69f978a45c8b
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-07-27 23:10:49 +0530

    Edited showreel and showreel params

--- src/components/ShowReel/ShowReel.jsx
+++ src/components/ShowReel/ShowReel.jsx
@@ -1,20 +1,20 @@
-import { FiMenu, FiArrowUpRight } from "react-icons/fi";
-import { useEffect, useRef, useState } from "react";
-import { useAnimate, motion } from "framer-motion";
-import ShowReelModal from "./ShowReelModal";
+import { FiMenu, FiArrowUpRight } from "react-icons/fi"
+import { useEffect, useRef, useState } from "react"
+import { useAnimate, motion } from "framer-motion"
+import ShowReelModal from "./ShowReelModal"
 
 const ShowReel = ({ viewRef, scale }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <>
       <motion.section
         ref={viewRef}
         style={{ scale }}
         onClick={() => {
-          document.body.style.overflow = "hidden";
-          setIsOpen(true);
+          document.body.style.overflow = "hidden"
+          setIsOpen(true)
         }}
-        className="z-40 relative w-[25vw] h-[25vh] overflow-hidden "
+        className="relative z-40 h-[25vh] w-[25vw] overflow-hidden"
       >
         {/* {!isOpen &&<Video />}  */}
         <Video isOpen={isOpen} />
@@ -22,38 +22,38 @@ const ShowReel = ({ viewRef, scale }) => {
       {/* {isOpen && <ShowReelModal isOpen={isOpen} setIsOpen={setIsOpen} />} */}
       <ShowReelModal isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
-  );
-};
+  )
+}
 
 const Video = ({ isOpen }) => {
-  const [hovered, setHovered] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [hovered, setHovered] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
 
-  const [scope, animate] = useAnimate();
-  const navRef = useRef(null);
+  const [scope, animate] = useAnimate()
+  const navRef = useRef(null)
 
   const handleMouseMove = ({ offsetX, offsetY, target }) => {
     // @ts-ignore
-    const isNavElement = [...target.classList].includes("glass-nav");
+    const isNavElement = [...target.classList].includes("glass-nav")
 
     if (isNavElement) {
-      setHovered(true);
+      setHovered(true)
 
-      const top = offsetY + "px";
-      const left = offsetX + "px";
+      const top = offsetY + "px"
+      const left = offsetX + "px"
 
-      animate(scope.current, { top, left }, { duration: 0 });
+      animate(scope.current, { top, left }, { duration: 0 })
     } else {
-      setHovered(false);
+      setHovered(false)
     }
-  };
+  }
 
   useEffect(() => {
-    navRef.current?.addEventListener("mousemove", handleMouseMove);
+    navRef.current?.addEventListener("mousemove", handleMouseMove)
 
     return () =>
-      navRef.current?.removeEventListener("mousemove", handleMouseMove);
-  }, []);
+      navRef.current?.removeEventListener("mousemove", handleMouseMove)
+  }, [])
 
   // let reel;
   // let player;
@@ -75,9 +75,9 @@ const Video = ({ isOpen }) => {
       style={{
         cursor: hovered ? "none" : "auto",
       }}
-      className="glass-nav w-full z-10 mx-auto max-w-6xl overflow-hidden h-full border-transparent  "
+      className="glass-nav z-10 mx-auto h-full w-full max-w-6xl overflow-hidden border-transparent"
     >
-      <div className="glass-nav flex items-center justify-between h-full w-full">
+      <div className="glass-nav flex h-full w-full items-center justify-between">
         <Cursor hovered={hovered} scope={scope} />
 
         <div
@@ -96,7 +96,9 @@ const Video = ({ isOpen }) => {
           <iframe
             title="reel"
             id="reel"
-            src="https://www.youtube-nocookie.com/embed/cnoM8scJZ98?si=DkhtAp6qt8s0wNrX&amp;controls=0&amp;mute=1&amp;autoplay=1&amp;loop=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" 
+            src="https://www.youtube-nocookie.com/embed/cnoM8scJZ98?si=DkhtAp6qt8s0wNrX&amp;playlist=cnoM8scJZ98&amp;controls=0&amp;mute=1&amp;autoplay=1&amp;loop=1&amp;disablekb=1"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
             // src="https://player.vimeo.com/video/938376141?h=4bb8442f8d&background=1&title=0&byline=0&muted=1&controls=0"
             // style={{ position:'relative',width: "25vw !important", height: "25vh !important",padding:0 }}
             style={{
@@ -141,8 +143,8 @@ const Video = ({ isOpen }) => {
         </div> */}
       </div>
     </div>
-  );
-};
+  )
+}
 
 const Cursor = ({ hovered, scope }) => {
   return (
@@ -150,16 +152,17 @@ const Cursor = ({ hovered, scope }) => {
       initial={false}
       animate={{
         opacity: hovered ? 1 : 0,
-        transform: `scale(${hovered ? 1 : 0
-          }) translateX(-50%) translateY(-50%)`,
+        transform: `scale(${
+          hovered ? 1 : 0
+        }) translateX(-50%) translateY(-50%)`,
       }}
       transition={{ duration: 0.15 }}
       ref={scope}
-      className="pointer-events-none z-40 absolute  grid h-[50px] w-[50px] origin-[0px_0px] place-content-center rounded-full bg-gradient-to-br from-primary from-40% to-gray-400 text-secondary1 text-sm"
+      className="pointer-events-none absolute z-40 grid h-[50px] w-[50px] origin-[0px_0px] place-content-center rounded-full bg-gradient-to-br from-primary from-40% to-gray-400 text-sm text-secondary1"
     >
       PLAY
     </motion.span>
-  );
-};
+  )
+}
 
-export default ShowReel;
+export default ShowReel
--- src/components/ShowReel/ShowReelModal.jsx
+++ src/components/ShowReel/ShowReelModal.jsx
@@ -1,5 +1,5 @@
-import { AnimatePresence, motion } from "framer-motion";
-import { FiAlertCircle } from "react-icons/fi";
+import { AnimatePresence, motion } from "framer-motion"
+import { FiAlertCircle } from "react-icons/fi"
 // import { useState } from "react";
 
 const ShowReelModal = ({ isOpen, setIsOpen }) => {
@@ -11,23 +11,22 @@ const ShowReelModal = ({ isOpen, setIsOpen }) => {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           onClick={() => {
-            document.body.style.overflowY = "scroll";
-            setIsOpen(false);
+            document.body.style.overflowY = "scroll"
+            setIsOpen(false)
           }}
-          className="bg-slate-900/20 backdrop-blur-xl p-8 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer overflow-x-hidden"
+          className="fixed inset-0 z-50 grid cursor-pointer place-items-center overflow-x-hidden overflow-y-scroll bg-slate-900/20 p-8 backdrop-blur-xl"
         >
           <motion.div
             initial={{ scale: 0, rotate: "12.5deg" }}
             animate={{ scale: 1, rotate: "0deg" }}
             exit={{ scale: 0, rotate: "0deg" }}
             // onClick={
-            //   (e) => 
+            //   (e) =>
             //     e.stopPropagation()
             // }
-            className=" text-white p-2 bg-primary
-             rounded-lg w-[800px] shadow-xl cursor-default relative "
+            className="relative w-[800px] cursor-default rounded-lg bg-primary p-2 text-white shadow-xl"
           >
-            <div className="relative z-10 w-full h-full object-cover">
+            <div className="relative z-10 h-full w-full object-cover">
               {/* <video
                 preload="none"
                 poster="/thumbnails/random3.png"
@@ -39,26 +38,47 @@ const ShowReelModal = ({ isOpen, setIsOpen }) => {
                 src="/videos/random3.mp4"
                 className="w-full hover:cursor-pointer h-full"
               ></video> */}
-              <div style={{position:'relative',paddingLeft:'50%', paddingBottom:'35%',paddingTop:'35%',paddingRight:'50%', width: "100%", height: "100%",overflowX:'hidden' }}
-          className="">
-          <iframe
-            src="https://player.vimeo.com/video/938376141?h=4bb8442f8d&autoplay=1&loop=1&title=0&byline=0&muted=0&controls=1"
-            // style={{ position:'relative',width: "25vw !important", height: "25vh !important",padding:0 }}
-            style={{position:"absolute",top:"0",left:"0",width:"100%",height:"100%"}}
-            // className="w-full h-full"
-            frameborder="0"
-            allow="autoplay; fullscreen; picture-in-picture"
-            webkitallowfullscreen 
-            mozallowfullscreen
-            allowfullscreen
-            ></iframe>
-            </div>
+              <div
+                style={{
+                  position: "relative",
+                  paddingLeft: "50%",
+                  paddingBottom: "35%",
+                  paddingTop: "35%",
+                  paddingRight: "50%",
+                  width: "100%",
+                  height: "100%",
+                  overflowX: "hidden",
+                }}
+                className=""
+              >
+                <iframe
+                  title="reelModal"
+                  // src="https://player.vimeo.com/video/938376141?h=4bb8442f8d&autoplay=1&loop=1&title=0&byline=0&muted=0&controls=1"
+                  src="https://www.youtube-nocookie.com/embed/cnoM8scJZ98?si=DkhtAp6qt8s0wNrX&amp;playlist=cnoM8scJZ98&amp;controls=1&amp;autoplay=1&amp;loop=1"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  // style={{ position:'relative',width: "25vw !important", height: "25vh !important",padding:0 }}
+                  style={{
+                    position: "absolute",
+                    top: "0",
+                    left: "0",
+                    width: "100%",
+                    height: "100%",
+                  }}
+                  // className="w-full h-full"
+                  frameBorder="0"
+                  // allow="autoplay; fullscreen; picture-in-picture"
+                  webkitallowfullscreen
+                  mozallowfullscreen
+                  allowfullscreen
+                ></iframe>
+              </div>
             </div>
           </motion.div>
         </motion.div>
       )}
     </AnimatePresence>
-  );
-};
+  )
+}
 
-export default ShowReelModal;
+export default ShowReelModal
