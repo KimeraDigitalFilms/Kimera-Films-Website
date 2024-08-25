238c9029ce45245db119297926748198cc3b5a15
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-08-25 20:52:56 +0530

    Rearranged and Renamed Project Gallery

--- src/components/Projects Masonry/GridComponent.jsx
+++ src/components/Projects Masonry/GridComponent.jsx
@@ -1,21 +1,18 @@
-import { useState, useRef } from "react";
-import React from "react";
-import List from "./List";
-import Modal from "./Modal";
-import Logo from "../Logo/Logo";
-
+import { useState, useRef } from "react"
+import React from "react"
+import List from "./List"
+import Modal from "./Modal"
+import Logo from "../Logo/Logo"
 
 function GridComponent() {
-  const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState(null)
 
- 
   return (
     <>
-      <div className="w-full my-5 ">
-      <div
-          className={`absolute w-full top-[100vh] h-[870vh] left-0 -z-40`}
-        >
-          <Logo src={'/logo.svg'}
+      <div className="my-5 w-full">
+        <div className={`absolute left-0 top-[100vh] -z-40 h-[940vh] w-full`}>
+          <Logo
+            src={"/logo.svg"}
             style={` bg-primary/60 w-[500px] sticky left-[30%] top-[25%] -z-40 `}
           />
         </div>
@@ -23,7 +20,7 @@ function GridComponent() {
         <Modal selected={selected} setSelected={setSelected} />
       </div>
     </>
-  );
+  )
 }
 
-export default GridComponent;
+export default GridComponent
--- src/components/Projects Masonry/List.jsx
+++ src/components/Projects Masonry/List.jsx
@@ -6,147 +6,153 @@ import "./List.css"
 function List({ setSelected }) {
   const list = [
     {
-      // vid: "https://www.youtube.com/embed/WWituJt63Nk?si=E_0VK8xBIahGiQzr",
-      vid: "https://www.youtube.com/embed/P1G1WaRVLDs?si=E8BH5trsbpUicpYP",
-      title: "Project 1",
-      poster: "/thumbnails/Project1.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/rOvfSnJDLVw?si=wyrp31prNJefGdhW",
-      title: "Project 2",
-      poster: "/thumbnails/Ramagya Second Video.00_00_00_00.Still001.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/ld3zekD5vCU?si=3-xFh7rkT-qH74kM",
-      // vid:"https://youtube.com/embed/lSbLpLFEglE?si=JK3rqfpr_FcOi0ks", //shorts
-      title: "Project 3",
-      poster: "/thumbnails/Dance.webp",
-    },
-    {
-      // vid: "https://player.vimeo.com/video/21066530?h=1183d78d8b",
-      vid: "https://www.youtube.com/embed/44Agu6gBS84?si=kJulzN-AqRVBH8uR",
-      title: "Project 4",
-      poster: "/thumbnails/Project4.webp",
-    },
-    {
-      // vid: "https://player.vimeo.com/video/394673541?h=3e4e45cb06",
-      vid: "https://www.youtube.com/embed/WWituJt63Nk?si=untMgr2ciiD9Qcpc",
-      title: "Project 5",
-      poster: "/thumbnails/Crossfit.webp",
-    },
-    {
-      // vid: "https://player.vimeo.com/video/253316833?h=0363f35d21",
-      vid: "https://www.youtube.com/embed/IQvC50eAUyc?si=fYiJ-3sMQ8KaXu8X",
-      title: "Project 6",
-      poster: "/thumbnails/Doggy 1.00_00_25_25.Still002.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/G5VODLMa0L8?si=kfwk_-Fqg0tSRSHd",
-      title: "Project 7",
-      poster:
-        "/thumbnails/Employee Hiring Final Render 1080p.00_02_30_23.Still001.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/tVGypPk4LBM?si=A0TlDX_R9fHofW47",
-      title: "Project 8",
-      poster: "/thumbnails/VW WITH CHANGES.00_01_06_04.Still002.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/oENRpM-CqXc?si=Tc8RIrt1ZdzjW2dn",
-      title: "Project 9",
-      poster: "/thumbnails/Ramagya.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/INiViDtjWoc?si=ZW6JWfhsob0mZ5SM",
-      title: "Project 10",
-      poster: "/thumbnails/Crossfit.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/liiiKGroM1s?si=ogK-Qpe-WxEbZnZJ",
-      title: "Project 11",
-      poster:
-        "/thumbnails/Candy_Ad_v033_ProRes4444 (1).00_00_28_15.Still001.webp",
-    },
-    {
-      // vid: "https://www.youtube.com/embed/P1G1WaRVLDs?si=owuRqnA9BsAoN4Ju",
       vid: "https://www.youtube.com/embed/JHtji0bru5Q?si=TAAeiHgMTAuA3pFY",
-      title: "Project 12",
-      poster: "/thumbnails/Keywest 2K.00_00_55_17.Still001.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/FeVfiTgfsyE?si=X2cxau-KyXZ6lzDN",
-      title: "Project 13",
-      poster: "/thumbnails/Project13.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/0MWLJoOxN-I?si=ulxqcZPJyR110E_b",
-      title: "Project 14",
-      poster: "/thumbnails/Project14.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/4vxrRiY4cNo?si=3X-1GkPqt8FVHgIj",
-      title: "Project 15",
-      poster: "/thumbnails/Project15.webp",
+      title: "Project 1",
+      poster: "/thumbnails/Keywest_Madhur.webp",
     },
     {
       vid: "https://www.youtube.com/embed/2_YFjkCWbPY?si=iQb3qdVJxxY4YSOs",
-      title: "Project 16",
-      poster: "/thumbnails/Project16.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/rJVnNfREh2c?si=-oiyoJ6YOl4trQEb", //ye sahi hai
-      title: "Project 17",
-      poster: "/thumbnails/Crossfit.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/NLcNFVAz06Q?si=w5I8rH5rvog9mLDT",
-      title: "Project 18",
-      poster: "/thumbnails/Pedal Tennis 1.00_00_14_15.Still002.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/CaeyUgFUEM8?si=WVtUwteedlXWLXzP",
-      title: "Project 19",
-      poster: "/thumbnails/Football Paid (1).00_00_24_09.Still001.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/t1s2buHQsMw?si=WgRkYWzG3xhbGdtc",
-      title: "Project 20",
-      poster: "/thumbnails/Cricket 4k.00_00_11_09.Still001.webp",
+      title: "Project 2",
+      poster: "/thumbnails/Hell.webp",
     },
     {
       vid: "https://www.youtube.com/embed/gvK_HBJOhZQ?si=qnN08VIJXxNUcC2k",
-      title: "Project 21",
+      title: "Project 3",
       poster: "/thumbnails/Pilates.webp",
     },
     {
-      vid: "https://www.youtube.com/embed/FEkP1gIjYrg?si=MjEm12mVfWagj9xw",
-      title: "Project 22",
+      vid: "https://www.youtube.com/embed/liiiKGroM1s?si=ogK-Qpe-WxEbZnZJ",
+      title: "Project 4",
+      poster: "/thumbnails/Candy.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/P1G1WaRVLDs?si=E8BH5trsbpUicpYP",
+      title: "Project 5",
+      poster: "/thumbnails/Keywest_Intro.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/tVGypPk4LBM?si=A0TlDX_R9fHofW47",
+      title: "Project 6",
+      poster: "/thumbnails/VW.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/V9003tH-8Q4?si=cwvsu4NyEmKJO8Ip",
+      title: "Project 7",
+      poster: "/thumbnails/Combined_Testimonial.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/INiViDtjWoc?si=ZW6JWfhsob0mZ5SM",
+      title: "Project 8",
+      poster: "/thumbnails/Sarvjan_Investor.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/ld3zekD5vCU?si=3-xFh7rkT-qH74kM",
+      title: "Project 9",
+      poster: "/thumbnails/Dance.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/0MWLJoOxN-I?si=ulxqcZPJyR110E_b",
+      title: "Project 10",
+      poster: "/thumbnails/Bobal.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/FeVfiTgfsyE?si=X2cxau-KyXZ6lzDN",
+      title: "Project 11",
+      poster: "/thumbnails/Wingreens.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/CaeyUgFUEM8?si=WVtUwteedlXWLXzP",
+      title: "Project 12",
+      poster: "/thumbnails/Football.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/rJVnNfREh2c?si=-oiyoJ6YOl4trQEb",
+      title: "Project 13",
       poster: "/thumbnails/Crossfit.webp",
     },
     {
-      vid: "https://www.youtube.com/embed/lafqwHugfEk?si=i7erhLRxxCIwRd_B",
+      vid: "https://www.youtube.com/embed/G5VODLMa0L8?si=kfwk_-Fqg0tSRSHd",
+      title: "Project 14",
+      poster: "/thumbnails/Sarvjan_Hiring.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/rOvfSnJDLVw?si=wyrp31prNJefGdhW",
+      title: "Project 15",
+      poster: "/thumbnails/Ramagya1.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/o495uJy2x2w?si=mlkYXrqofualstkN",
+      title: "Project 16",
+      poster: "/thumbnails/Influencer.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/WWituJt63Nk?si=untMgr2ciiD9Qcpc",
+      title: "Project 17",
+      poster: "/thumbnails/Reel 2 Foundation.00_00_38_42.Still001.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/IQvC50eAUyc?si=fYiJ-3sMQ8KaXu8X",
+      title: "Project 18",
+      poster: "/thumbnails/Doggy1.webp",
+    },
+
+    {
+      vid: "https://www.youtube.com/embed/oENRpM-CqXc?si=Tc8RIrt1ZdzjW2dn",
+      title: "Project 19",
+      poster: "/thumbnails/Ramagya2.webp",
+    },
+
+    {
+      vid: "https://www.youtube.com/embed/4vxrRiY4cNo?si=3X-1GkPqt8FVHgIj",
+      title: "Project 20",
+      poster: "/thumbnails/Habanero.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/9QRWpmsx3Ws?si=WOuOu0KmkX_QjUb0",
+      title: "Project 21",
+      poster: "/thumbnails/Ladies 1.00_00_39_01.Still001.webp",
+    },
+
+    {
+      vid: "https://www.youtube.com/embed/t1s2buHQsMw?si=WgRkYWzG3xhbGdtc",
+      title: "Project 22",
+      poster: "/thumbnails/Cricket.webp",
+    },
+
+    {
+      vid: "https://www.youtube.com/embed/FEkP1gIjYrg?si=MjEm12mVfWagj9xw",
       title: "Project 23",
+      poster: "/thumbnails/Doggy2.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/lafqwHugfEk?si=i7erhLRxxCIwRd_B",
+      title: "Project 24",
       poster: "/thumbnails/Ladies 3.00_00_26_27.Still001.webp",
     },
     {
       vid: "https://www.youtube.com/embed/7ev0oJxYnN8?si=mhahyPiMGFoYKpsu",
-      title: "Project 24",
-      poster: "/thumbnails/Crossfit.webp",
+      title: "Project 25",
+      poster: "/thumbnails/Doggy3.webp",
     },
     {
       vid: "https://www.youtube.com/embed/nTUEDnHsxvA?si=E8veK-MIHzUaMVjM",
-      title: "Project 25",
-      poster: "/thumbnails/Crossfit.webp",
+      title: "Project 26",
+      poster: "/thumbnails/PTM.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/44Agu6gBS84?si=kJulzN-AqRVBH8uR",
+      title: "Project 27",
+      poster: "/thumbnails/Ramagya3.webp",
+    },
+    {
+      vid: "https://www.youtube.com/embed/NLcNFVAz06Q?si=w5I8rH5rvog9mLDT",
+      title: "Project 28",
+      poster: "/thumbnails/Tennis.webp",
     },
     {
       vid: "https://www.youtube.com/embed/aAmCkozIr-I?si=765uXYOZdTwGvlc1",
-      title: "Project 26",
-      poster: "/thumbnails/Ladies 2.00_00_15_14.Still001.webp",
-    },
-    {
-      vid: "https://www.youtube.com/embed/9QRWpmsx3Ws?si=WOuOu0KmkX_QjUb0",
-      title: "Project 27",
-      poster: "/thumbnails/Crossfit.webp",
+      title: "Project 29",
+      poster: "/thumbnails/Ramagya4.webp",
     },
   ]
 
@@ -154,31 +160,31 @@ function List({ setSelected }) {
     <>
       <motion.div
         id="grid"
-        className="screen-padding h-[870vh] w-full gap-x-4 gap-y-7"
+        className="screen-padding h-[940vh] w-full gap-x-4 gap-y-7"
         // className="columns-1 md:columns-2 xl:columns-3  gap-x-0"
       >
         <Card setSelected={setSelected} item={list[0]} style={"col-span-4"} />
+        <Card setSelected={setSelected} item={list[1]} style={"col-span-2"} />
         <Card
           setSelected={setSelected}
-          item={list[1]}
+          item={list[2]}
           style={"col-span-6 col-start-9 row-start-2"}
         />
-        <Card setSelected={setSelected} item={list[2]} style={"col-span-2"} />
 
         <Card
           setSelected={setSelected}
           item={list[3]}
-          style={"col-span-5 col-start-8 row-start-11"}
-        />
-        <Card
-          setSelected={setSelected}
-          item={list[4]}
           style={"col-span-3 col-start-5 row-start-6"}
         />
         <Card
           setSelected={setSelected}
-          item={list[5]}
+          item={list[4]}
           style={"col-span-3 col-start-1 row-start-7"}
+        />
+        <Card
+          setSelected={setSelected}
+          item={list[5]}
+          style={"col-span-5 col-start-8 row-start-11"}
         />
         <Card
           setSelected={setSelected}
@@ -233,7 +239,7 @@ function List({ setSelected }) {
         <Card
           setSelected={setSelected}
           item={list[16]}
-          style={"col-span-4 row-start-[36] col-start-2"}
+          style={"col-span-4 row-start-[36] col-start-5"}
         />
         <Card
           setSelected={setSelected}
@@ -283,7 +289,17 @@ function List({ setSelected }) {
         <Card
           setSelected={setSelected}
           item={list[26]}
-          style={"col-span-5 row-start-[56] col-start-4"}
+          style={"col-span-3 row-start-[56] col-start-2"}
+        />
+        <Card
+          setSelected={setSelected}
+          item={list[27]}
+          style={"col-span-4 row-start-[59] col-start-8"}
+        />
+        <Card
+          setSelected={setSelected}
+          item={list[28]}
+          style={"col-span-2 row-start-[61] col-start-3"}
         />
       </motion.div>
     </>
